5b6b062239287953ef9530edca5d4434596fd597
Rami-Ismael <rami.m.ismael@gmail.com>
2024-06-18 22:17:58 -0500

    Refactor time condition in Environment class to check for a multiple of 8196 instead of 2

--- pokegym/environment.py
+++ pokegym/environment.py
@@ -781,7 +781,7 @@ class Environment(Base):
 
         info = {}
         done = self.time >= self.max_episode_steps
-        if self.time % ( 131072 // 2 )  == 0 or done:
+        if self.time % ( 131072 // 8 )  == 0 or done:
             info = {
                 'reward': {
                     'reward': reward,
