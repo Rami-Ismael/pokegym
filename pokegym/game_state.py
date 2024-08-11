95643ae30d4703559705a5c1839b402498cfe995
Rami-Ismael <rami.m.ismael@gmail.com>
2024-08-10 19:58:57 -0500

    Refactor game_state.py to validate enemy selected move ID and update assertion error message

--- pokegym/game_state.py
+++ pokegym/game_state.py
@@ -264,6 +264,8 @@ class Internal_Game_State:
             assert element <= 415 # https://www.psypokes.com/rby/maxstats.php
         if self.player_selected_move_id is not None and self.player_selected_move_id > 166:
             raise ValueError(f"Player Selected Move ID is not valid and is not in the range of 0-166 the move is {self.player_selected_move_id}")
+        if self.enemy_selected_move_id is not None and self.enemy_selected_move_id > 166:
+            raise ValueError(f"Enemy Selected Move ID is not valid and is not in the range of 0-166 the move is {self.enemy_selected_move_id}")
         if   self.enemy_current_move_effect is not None and self.enemy_current_move_effect > 56:
             raise ValueError(f"Enemy Current Move Effect is not valid and is not in the range of 0-56 the move is {self.enemy_current_move_effect}")
         if self.enemy_pokemon_move_type is not None and self.enemy_pokemon_move_type > 15:
