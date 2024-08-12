Refactor environment.py to fix off-by-one error in episode selection probability

pokegym/environment.py
@@ -440,9 +440,10 @@ class Environment(Base):
             assert self.time ==0 , T()
             def reduce_selection_probability_base_on_how_close_it_start_state():
                 list_of_probability_of_selection = []
-                list_of_number = [0]
+                list_of_number = []
+                n = len(self.go_explored_list_of_episodes)
                 for i in range(1 , len(self.go_explored_list_of_episodes)+1):
-                    list_of_probability_of_selection.append(i / len(self.go_explored_list_of_episodes))
+                    list_of_probability_of_selection.append(i / ( ( n * (n + 1) ) / 2 ))
                     list_of_number.append(i-1)
                 self.random_number = np.random.choice(list_of_number, p=list_of_probability_of_selection)
                 self.external_game_state = self.go_explored_list_of_episodes[self.random_number]["external_game_state"]